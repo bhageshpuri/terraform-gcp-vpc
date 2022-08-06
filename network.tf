06b2adae4bb2082719d51d9c4fe2f1658b83ba57
Your Name <you@example.com>
2022-08-06 11:23:42 +0530

    FIrst commit

---
+++ network.tf
@@ -0,0 +1,20 @@
+module "vpc" {
+  source  = "terraform-google-modules/network/google"
+  version = "~> 3.2.2"
+
+  project_id   = var.project_id
+  network_name = var.network_name
+  routing_mode = "GLOBAL"
+
+  subnets = [
+    {
+      subnet_name           = var.subnet_name
+      subnet_ip             = "10.10.20.0/24"
+      subnet_region         = "us-central1"
+      subnet_private_access = "true"
+      subnet_flow_logs      = "true"
+      description           = "This subnet has a description"
+    },
+      ]
+}
+
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+ default = "us-central1"
+}
+
+variable "zone" {
+ default = "us-central1-a"
+}
+
+variable "project_id" {
+ default = "helical-ascent-350416"
+}
+
+variable "network_name" {
+  default = "tf-vpc-703246"
+}
+
+variable "subnet_name" {
+  default = "subnet-01"
+}
